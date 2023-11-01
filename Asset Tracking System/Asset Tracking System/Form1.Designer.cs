4a4beb30fa065eda3a26b0541b11944edcb06a91
Gintoki734 <141349536+Gintoki734@users.noreply.github.com>
2023-11-01 20:22:55 +0000

    Created classes and function to add asset

--- Asset Tracking System/Asset Tracking System/Form1.Designer.cs
+++ Asset Tracking System/Asset Tracking System/Form1.Designer.cs
@@ -35,10 +35,10 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(253, 139);
+            dataGridView1.Location = new Point(12, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(360, 225);
+            dataGridView1.Size = new Size(1273, 532);
             dataGridView1.TabIndex = 0;
             // 
             // Form1
