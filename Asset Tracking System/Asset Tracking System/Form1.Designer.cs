79ecea4a74c3da5a2c87e5f849b8b81cd8aefd7c
khana <khana@LAPTOP-6LGGV5F8>
2023-12-15 17:24:21 +0000

    Added label because of the feeback received from the user

--- Asset Tracking System/Asset Tracking System/Form1.Designer.cs
+++ Asset Tracking System/Asset Tracking System/Form1.Designer.cs
@@ -35,13 +35,15 @@
             btnV = new Button();
             richTextBox1 = new RichTextBox();
             btnEnd = new Button();
+            label4 = new Label();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // btnEdit
             // 
             btnEdit.Font = new Font("Segoe UI Semilight", 26F, FontStyle.Italic, GraphicsUnit.Point);
-            btnEdit.Location = new Point(428, 697);
+            btnEdit.Location = new Point(428, 727);
             btnEdit.Margin = new Padding(2);
             btnEdit.Name = "btnEdit";
             btnEdit.Size = new Size(270, 106);
@@ -53,7 +55,7 @@
             // dataGridView2
             // 
             dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Location = new Point(18, 16);
+            dataGridView2.Location = new Point(18, 96);
             dataGridView2.Margin = new Padding(2);
             dataGridView2.Name = "dataGridView2";
             dataGridView2.RowHeadersWidth = 62;
@@ -66,7 +68,7 @@
             // btnAddAsset
             // 
             btnAddAsset.Font = new Font("Segoe UI Semilight", 26F, FontStyle.Italic, GraphicsUnit.Point);
-            btnAddAsset.Location = new Point(18, 697);
+            btnAddAsset.Location = new Point(18, 727);
             btnAddAsset.Margin = new Padding(2);
             btnAddAsset.Name = "btnAddAsset";
             btnAddAsset.Size = new Size(270, 106);
@@ -78,7 +80,7 @@
             // btnDelete
             // 
             btnDelete.Font = new Font("Segoe UI Semilight", 26F, FontStyle.Italic, GraphicsUnit.Point);
-            btnDelete.Location = new Point(813, 697);
+            btnDelete.Location = new Point(813, 727);
             btnDelete.Margin = new Padding(2);
             btnDelete.Name = "btnDelete";
             btnDelete.Size = new Size(302, 106);
@@ -90,7 +92,7 @@
             // btnV
             // 
             btnV.Font = new Font("Segoe UI Semilight", 26F, FontStyle.Italic, GraphicsUnit.Point);
-            btnV.Location = new Point(1218, 697);
+            btnV.Location = new Point(1218, 727);
             btnV.Margin = new Padding(2);
             btnV.Name = "btnV";
             btnV.Size = new Size(382, 106);
@@ -101,8 +103,8 @@
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(1702, 16);
-            richTextBox1.Margin = new Padding(2, 2, 2, 2);
+            richTextBox1.Location = new Point(1702, 96);
+            richTextBox1.Margin = new Padding(2);
             richTextBox1.Name = "richTextBox1";
             richTextBox1.Size = new Size(333, 609);
             richTextBox1.TabIndex = 6;
@@ -111,7 +113,7 @@
             // btnEnd
             // 
             btnEnd.Font = new Font("Segoe UI Semilight", 26F, FontStyle.Italic, GraphicsUnit.Point);
-            btnEnd.Location = new Point(1810, 697);
+            btnEnd.Location = new Point(1810, 727);
             btnEnd.Margin = new Padding(2);
             btnEnd.Name = "btnEnd";
             btnEnd.Size = new Size(223, 106);
@@ -120,11 +122,35 @@
             btnEnd.UseVisualStyleBackColor = true;
             btnEnd.Click += btnEnd_Click;
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(18, 21);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(978, 37);
+            label4.TabIndex = 37;
+            label4.Text = "Please select the data row you want to delete or edit before clicking the button";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(1702, 21);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(253, 37);
+            label1.TabIndex = 38;
+            label1.Text = "Vulnerability Result";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1861, 844);
+            Controls.Add(label1);
+            Controls.Add(label4);
             Controls.Add(btnEnd);
             Controls.Add(richTextBox1);
             Controls.Add(btnV);
@@ -138,6 +164,7 @@
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -148,5 +175,7 @@
         private Button btnV;
         private RichTextBox richTextBox1;
         private Button btnEnd;
+        private Label label4;
+        private Label label1;
     }
 }
